9306ae3b238220855340325af5e1d6f81bca83c1
Apricity <101598622+JustApricity@users.noreply.github.com>
2023-02-08 11:35:18 -0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
+router.get('/courses/add', courseController.renderAddForm);
+router.post('/courses/add', courseController.addCourse);
+router.post('/courses/delete/:id', courseController.deleteCourse);
 module.exports = router;
